411a2977b5fbfba285ec9e9b87bab3d133e69248
Madison Kenney <mbk81@pitt.edu>
2026-04-12 20:48:22 -0400

    implemented find start

--- project4/pathfinder.cpp
+++ project4/pathfinder.cpp
@@ -6,6 +6,12 @@
 #include "List.hpp"
 #include "Queue.hpp"
 
+// Struct used to store locations of specific pixels in the maze
+struct State {
+  int row;
+  int col;
+};
+
 int main(int argc, char *argv[])
 {
   // get input/output file names from command line arguments
@@ -21,8 +27,30 @@ int main(int argc, char *argv[])
 
   // Read input image from file
   Image image = readFromFile(input_file);
+  int rows = image.height();
+  int cols = image.width();
+
+  // Find start pixel (red)
+  State start{-1, 1};
+  int startCount = 0;
+
+  for(u_char i=0; i<rows; i++){
+    for(u_char j=0; j<cols; j++){
+      if(image(i, j) == RED){
+        start = {i, j};
+        startCount++;
+      }
+    }
+  }
+
+  if(startCount == 0){        // no start found
+    return EXIT_FAILURE;
+  } else if(startCount > 1){  // more than 1 start found
+    return EXIT_FAILURE;
+  }
 
   // TODO: breadth-first search
+  
 
   // TODO: Write solution image to file
   
